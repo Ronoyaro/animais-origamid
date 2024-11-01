52d0619f96e5e90e485bdb6fde6c621f63d8a7be
Ronoyaro <ronaldferreiraig@hotmail.com>
2024-11-01 15:23:35 -0300

    accordion refatorado para classe

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,13 +1,21 @@
-export default function initAccordion() {
-  const questions = document.querySelectorAll(".js-accordion dt");
-  const response = document.querySelector("dd").classList.add("ativo");
+export default class Accordion {
+  constructor(lists) {
+    this.lists = document.querySelectorAll(lists);
+  }
 
-  if (questions.length) {
-    function accordionList() {
-      this.nextElementSibling.classList.add("ativo");
+  toggleAccordionEvent(item) {
+    item.classList.toggle("ativo");
+    item.nextElementSibling.classList.toggle("ativo");
+  }
+
+  init() {
+    if (this.lists.length) {
+      this.lists.forEach((item) => {
+        item.addEventListener("click", () => {
+          this.toggleAccordionEvent(item);
+        });
+      });
+      this.toggleAccordionEvent(this.lists[0]);
     }
-    questions.forEach((quest) => {
-      quest.addEventListener("click", accordionList);
-    });
   }
 }
--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 //Navegação por Tab
 import { ScrollToView } from "./modules/scrollToView.js";
+import Accordion from "./modules/accordion.js";
 import showModal from "./modules/modal.js";
 import initTabNav from "./modules/nav-tab.js";
 import AnimaScroll from "./modules/animaScroll.js";
-import initAccordion from "./modules/accordion.js";
 import initTooltip from "./modules/tooltip.js";
 import dropDownMenu from "./modules/dropdown-menu.js";
 import menuMobile from "./modules/menu-mobile.js";
@@ -14,7 +14,6 @@ import fetchBitcoin from "./modules/fetchBitcoin.js";
 showModal();
 initTabNav();
 AnimaScroll();
-initAccordion();
 initTooltip();
 dropDownMenu();
 menuMobile();
@@ -23,5 +22,7 @@ fetchAnimais();
 fetchBitcoin();
 
 const scrollToView = new ScrollToView('.js-menu a[href^="#"]');
-
 scrollToView.init();
+
+const accordion = new Accordion(".js-accordion dt");
+accordion.init();
